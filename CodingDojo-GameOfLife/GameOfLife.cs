ddef064a83eba7a6b4e3a0721829e7691540e6f9
Jérémy Hure <hure.j@sfeir.com>
2022-10-20 15:55:36 +0200

    GameOfLife Works

--- CodingDojo-GameOfLife/GameOfLife.cs
+++ CodingDojo-GameOfLife/GameOfLife.cs
@@ -10,7 +10,43 @@ namespace CodingDojo_GameOfLife
     {
         public static bool[,] NextGen(bool[,] grid)
         {
-            return new bool[grid.GetLength(0), grid.GetLength(1)];
+            bool[,] newGrid = new bool[grid.GetLength(0), grid.GetLength(1)];
+            for (int i = 0; i < grid.GetLength(0); i++)
+            {
+                for (int j = 0; j < grid.GetLength(1); j++)
+                {
+                    int neigh = GetNeighbour(grid, i, j);
+                    if (grid[i, j])
+                    {
+                        if (neigh == 2 || neigh == 3)
+                        {
+                            newGrid[i, j] = true;
+                        }
+                    }
+                    else
+                    {
+                        if (neigh == 3)
+                        {
+                            newGrid[i, j] = true;
+                        }
+                    }
+                }
+            }
+            return newGrid;
+        }
+
+        private static int GetNeighbour(bool[,] grid, int i, int j)
+        {
+            int count = 0;
+            if (i != 0 && j != 0 && grid[i - 1, j - 1]) { count++; }
+            if (i != 0 && grid[i - 1, j]) { count++; }
+            if (i != 0 && j != grid.GetLength(1) - 1 && grid[i - 1, j + 1]) { count++; }
+            if (j != 0 && grid[i, j - 1]) { count++; }
+            if (j != grid.GetLength(1) - 1 && grid[i, j + 1]) { count++; }
+            if (i != grid.GetLength(0) - 1 && j != 0 && grid[i + 1, j - 1]) { count++; }
+            if (i != grid.GetLength(0) - 1 && grid[i + 1, j]) { count++; }
+            if (i != grid.GetLength(0) - 1 && j != grid.GetLength(1) - 1 && grid[i + 1, j + 1]) { count++; }
+            return count;
         }
     }
 }
